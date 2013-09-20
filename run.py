931a1948a0302d8325f1e5bf37dec2ce6ebe5790
MaTachi <dannie92@gmail.com>
2013-09-20 17:54:44 +0200

    Possible to choose IP address when starting run.py

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python2
 
+import sys
 from src.app import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    host = sys.argv[1] if len(sys.argv) > 1 else '127.0.0.1'
+    app.run(debug=True, host=host)
